a09cba92bad3f58cf774c282226e288fc41421e8
Nacho <ntorresco@gmail.com>
2018-11-21 16:00:10 -0300

    Control de usuarios no terminado

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
     before_save { self.email = email.downcase }
-    validates :name,  presence: true
+    #validates :name,  presence: true
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 }, 
     format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }  
--- db/migrate/20181109201352_create_users.rb
+++ db/migrate/20181109201352_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :name
       t.string :last_name
-      t.string :email
+  
       t.string :password
       t.string :genre
       t.integer :age
