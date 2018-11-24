92cab80259252320200802127469ef0f330c32e8
Nacho <ntorresco@gmail.com>
2018-11-23 22:39:23 -0300

    alta y baja de premium

--- db/migrate/20181124001151_create_user_types.rb
+++ db/migrate/20181124001151_create_user_types.rb
@@ -1,9 +1,7 @@
 class CreateUserTypes < ActiveRecord::Migration[5.1]
   def change
     create_table :user_types do |t|
-      t.string :userType, default: "2"
-      
-
+      t.string :userType 
       t.timestamps
     end
   end
