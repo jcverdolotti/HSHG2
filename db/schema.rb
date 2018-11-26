af1812da0128ea44c250507fc8e5ee59b0fa5d04
Juan <jcverdolotti@gmail.com>
2018-11-26 17:27:59 -0300

    vista del edit de user

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20181126142852) do
 
   create_table "petitions", force: :cascade do |t|
     t.string "email"
-    t.string "current_uType"
+    t.integer "current_uType"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
